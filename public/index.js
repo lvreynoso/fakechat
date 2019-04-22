09585a394e75c56da81c4d5c79362ed47b8d5c77
Lucia Reynoso <lvreynoso@protonmail.com>
2019-04-21 20:21:57 -0700

    Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -2,18 +2,54 @@ window.onload = function () {
     // connect to the socket.io server
     const socket = io.connect()
 
+    let currentUser = ''
+
     document.getElementById('createUserBtn').addEventListener('click', (event) => {
-        console.log('button clicked')
         event.preventDefault()
         const username = document.getElementById('usernameInput').value
         if (username.length > 0) {
             // emit to the server the new user
             socket.emit('new user', username)
+            currentUser = username
             document.getElementById('usernameForm').remove()
+            document.getElementById('mainContainer').style.display = 'flex' 
+        }
+    })
+
+    document.getElementById('sendChatBtn').addEventListener('click', (event) => {
+        event.preventDefault()
+        const message = document.getElementById('chatInput').value
+        if (message.length > 0) {
+            socket.emit('new message', {
+                sender: currentUser,
+                message: message,
+            })
+            document.getElementById('chatInput').value = ''
         }
     })
 
     socket.on('new user', (username) => {
         console.log(`✋ ${username} has joined the chat! ✋`)
+        let newUserDiv = document.createElement('div')
+        newUserDiv.className = 'userOnline'
+        newUserDiv.textContent = `${username}`
+        document.getElementById('usersOnline').appendChild(newUserDiv)
+    })
+
+    socket.on('new message', (data) => {
+        let senderNode = document.createElement('p')
+        senderNode.className = 'messageUser'
+        senderNode.textContent = `${data.sender}: `
+
+        let messageNode = document.createElement('p')
+        messageNode.className = 'messageText'
+        messageNode.textContent = `${data.message}`
+
+        let containerNode = document.createElement('div')
+        containerNode.className = 'message'
+        containerNode.appendChild(senderNode)
+        containerNode.appendChild(messageNode)
+
+        document.getElementById('messageContainer').appendChild(containerNode)
     })
 }
--- source/sockets/chat.js
+++ source/sockets/chat.js
@@ -1,9 +1,17 @@
 // chat.js
 
 const chat = function(io, socket) {
+    // Listen for new users
     socket.on('new user', (username) => {
         console.log(`${username} has joined the chat! ✋`)
         io.emit('new user', username)
+    })
+
+    // Listen for new messages
+    socket.on('new message', (data) => {
+        // Send that data back to ALL clients
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
     })
 }
 
